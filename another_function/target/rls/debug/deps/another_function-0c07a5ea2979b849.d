31d8ed3dd02667c7842d0775f81c2ef5534ddeae
yaoxin <yaoxinjing517@gmail.com>
2022-08-08 07:16:04 -0700

    chapter 3 functions

---
+++ another_function/target/rls/debug/deps/another_function-0c07a5ea2979b849.d
@@ -0,0 +1,5 @@
+/home/yaoxin/rust_learning/another_function/target/rls/debug/deps/another_function-0c07a5ea2979b849.rmeta: src/main.rs
+
+/home/yaoxin/rust_learning/another_function/target/rls/debug/deps/another_function-0c07a5ea2979b849.d: src/main.rs
+
+src/main.rs:
---
+++ another_function/target/rls/debug/deps/another_function-f04fe16579db7533.d
@@ -0,0 +1,5 @@
+/home/yaoxin/rust_learning/another_function/target/rls/debug/deps/another_function-f04fe16579db7533.rmeta: src/main.rs
+
+/home/yaoxin/rust_learning/another_function/target/rls/debug/deps/another_function-f04fe16579db7533.d: src/main.rs
+
+src/main.rs:
