476f871bfb6c582285fbb8aed3dfd34645e5cde4
yaoxin <yaoxinjing517@gmail.com>
2022-08-08 08:27:21 -0700

    add branches

---
+++ branches/target/rls/debug/deps/branches-403cdaeadc71b0f5.d
@@ -0,0 +1,5 @@
+/home/yaoxin/rust_learning/branches/target/rls/debug/deps/branches-403cdaeadc71b0f5.rmeta: src/main.rs
+
+/home/yaoxin/rust_learning/branches/target/rls/debug/deps/branches-403cdaeadc71b0f5.d: src/main.rs
+
+src/main.rs:
---
+++ branches/target/rls/debug/deps/branches-f05f12b36315bb26.d
@@ -0,0 +1,5 @@
+/home/yaoxin/rust_learning/branches/target/rls/debug/deps/branches-f05f12b36315bb26.rmeta: src/main.rs
+
+/home/yaoxin/rust_learning/branches/target/rls/debug/deps/branches-f05f12b36315bb26.d: src/main.rs
+
+src/main.rs:
