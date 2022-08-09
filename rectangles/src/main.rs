4cb2038b714e896b82e551b9c8d5e8072cb15082
yaoxin <yaoxinjing517@gmail.com>
2022-08-09 02:17:32 -0700

    use struct for area func and add debug point

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,22 +1,25 @@
-
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
-    println! (
+    println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+    
+    println!("rect1 is {:?}", rect1);
 
+    dbg!(&rect1);
 }
-
 // here fn doesn't take the ownership of dimensions, insead it copy the value in rect1 to
 // new tuple dimensions, since dimensions only contain scalar variable
 fn area(rectangle: &Rectangle) -> u32 {
