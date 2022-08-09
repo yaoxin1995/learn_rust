cdcbc1ff7de133b79190bad95258c709748f6200
yaoxin <yaoxinjing517@gmail.com>
2022-08-09 01:51:09 -0700

    use tuple for funtion area

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,14 +1,16 @@
 
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
 
     println! (
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(rect1)
     );
+
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+// here fn doesn't take the ownership of dimensions, insead it copy the value in rect1 to
+// new tuple dimensions, since dimensions only contain scalar variable
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
