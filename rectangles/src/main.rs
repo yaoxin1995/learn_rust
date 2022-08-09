8902728d73ecaf36efd2c444210f940d6a005f87
yaoxin <yaoxinjing517@gmail.com>
2022-08-09 01:55:36 -0700

    use struct for funtion area

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,16 +1,24 @@
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println! (
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
 
 }
 
 // here fn doesn't take the ownership of dimensions, insead it copy the value in rect1 to
 // new tuple dimensions, since dimensions only contain scalar variable
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
