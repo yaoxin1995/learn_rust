c21bca408eb44300bf2acc8d9f613e390c5ae337
yaoxin <yaoxinjing517@gmail.com>
2022-08-12 01:14:20 -0700

    use copy as trait bound instead of copy

---
+++ largest/src/main.rs
@@ -0,0 +1,26 @@
+
+fn largest<T: PartialOrd + Clone + std::fmt::Debug>(list: &[T]) -> T {
+    let mut largest = list[0].clone();
+
+    assert_eq!(largest, list[0].clone());
+
+    for item in list {
+        if item > &largest {
+            largest = item.clone();
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
